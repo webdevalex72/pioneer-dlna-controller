33ac5962ad4ef8254db46454393408c49f8ad564
Aleks <web.dev.alex72@gmail.com>
2026-05-30 18:48:04 +0300

    feat: integrate capacitor, add android platform and absolute socket config

---
+++ android/app/src/main/java/com/pioneer/controller/MainActivity.java
@@ -0,0 +1,20 @@
+package com.pioneer.controller;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Разрешаем смешанный контент (HTTPS приложение -> HTTP локальный сервер)
+        WebView webView = this.getBridge().getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+    }
+}
